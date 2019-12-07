9978fcd77a2f388127fe96173b42161e8ae62f22
Christopher F Carroll <chris.carroll@unforgettable.me.uk>
2019-12-07 16:12:21 +0000

    NetBSD remove redundant stuff

--- NetBSD/netbsd-install-bash-vim-net-etc-desktop.sh
+++ NetBSD/netbsd-install-bash-vim-net-etc-desktop.sh
@@ -1,2 +1,2 @@
 which pkgin || echo 'ERROR: pkgin not found. Install it as part of the initial system installation.'
-pkgin -yV install tmux sudo bash vim nano nmap iftop lsof mhash nbtscan netcat
+pkgin -yV install tmux sudo bash vim nano nmap iftop mhash nbtscan netcat
