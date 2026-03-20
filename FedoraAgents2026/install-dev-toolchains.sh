b5948c3741e516ba1a6a2075c161241ac2537b75
Agent F for Chris F Carroll <agentf@unforgettable.me.uk>
2026-03-20 20:08:42 +0000

    wip:FedoraAgents2026/*:shell preferences

--- FedoraAgents2026/install-dev-toolchains.sh
+++ FedoraAgents2026/install-dev-toolchains.sh
@@ -1,5 +1,4 @@
 #! /bin/sh
-sudo dnf install -y python uv node npm
-wget https://dot.net/v1/dotnet-install.sh -O /tmp/dotnet-install.sh
+sudo dnf install -y python uv node npm dotnet-sdk-10.0
 chmod +x /tmp/dotnet-install.sh
 /tmp/dotnet-install.sh --version latest
